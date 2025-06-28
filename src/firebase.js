12ef584984d74e68ba1e46cdd5cdf8a328db69a2
INDUWARA90 <induwaravishwakantha90@gmail.com>
2025-06-28 10:53:34 +0530

    Add firbase Configuration

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjolYNy4d14Dmih9MlipUFHEC-PPE1-Z8",
+  authDomain: "fot-canteen.firebaseapp.com",
+  projectId: "fot-canteen",
+  storageBucket: "fot-canteen.firebasestorage.app",
+  messagingSenderId: "187925673644",
+  appId: "1:187925673644:web:42f76109efbc8a2bad5109"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
